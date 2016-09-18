6b701496670389331fae50010865b1dd51d2542e
Jordan Deutsch <jordanbdeutsch@gmail.com>
2016-09-17 22:49:00 -0400

    safari flex box fixes

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
 
 
 gulp.task('sass', function () { 
@@ -7,6 +8,10 @@ gulp.task('sass', function () {
         .pipe(sass({
         	errLogToConsole: true
         }))
+        .pipe(autoprefixer({
+                browsers: ['last 2 versions'],
+                cascade: false
+         }))
         .pipe(gulp.dest(''));
 });
 
